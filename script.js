8800fdb0990abbb036eaebeb9bb0b4f832120fe9
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2024-10-05 12:29:51 +1000

    first

---
+++ script.js
@@ -0,0 +1,10 @@
+//Escape Character(\)
+
+const str = "Hi, I'm \"Ram\"";
+console.log(str);
+
+//another case scenario
+const str1 = 'Hi, I\'m "Ram" and I\\live in Sydney';
+
+//length of the string
+
