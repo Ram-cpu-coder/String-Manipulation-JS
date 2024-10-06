e58a61f8e4bd3468faab53a668f9f6ddf0494671
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2024-10-06 13:49:00 +1100

    update on README

--- README.md
+++ README.md
@@ -15,6 +15,8 @@ Here in this tutorial, I have learnt about
 12. str.length;
 13. str.replace("letter", "letter");
 14. str.replaceAll("letter", "letter");
+15. Escape character (\)
+16. str.substring(starting i, ending i);
 
 
 str[i]: 
@@ -59,3 +61,48 @@ this helps us to replace the letter a with b in the given string data. if there 
 
 str.replaceAll("a", "b"): 
 this helps us to replace all the a 's in the given string to b 's.
+
+Escape character (\):
+this helps us to use both of the quotation marks in the sring if we use it before the starting of the quotation marks and the before the ending of the quotation marks.
+
+str.substring(starting i, ending i): 
+this is similar to the slice method and does the same thing. it also has the starting point with the value of certain index and the ending point of the index with in the string.
+
+
+
+
+MORE TO GO:
+1. trim()
+2. split("")
+3. join()
+4. concat()
+
+str.trim():
+this is used to remove the space used before the string and after the string.
+this is used because in many cases we dont have the access to the data first until and unless the user inputs it 
+the user might put in the spaces before and after the string
+so after while logging they might not put the spaces, which leads to the unmatching of the string given by the user to our database and it will not work
+so deal with this problem this is used
+
+str.split(""):
+this is used to change the given string to the array 
+this is used to split the string 
+here we can see there is no space in between the quotation marks, that means it will split the string in every characters separated by commas 
+
+str.split(" "):
+this has the space which means it is splitting the string in every words. 
+
+str.split("any character"):
+this is splitting the string in every points where it finds that particular character.
+
+splitted.join():
+this is joining back the splitted string and changing it back to the string from an aray 
+
+splitted.join("a"):
+we can join the splitted string back by the character given as well. 
+
+
+concat():
+similar to the normal concatation 
+it is joining the strings 
+syntax: str.concat(" another sentence or word or anything");
--- script.js
+++ script.js
@@ -41,7 +41,7 @@ console.log(val4);
 
 //replace
 
-const replace = "My name is Ram Kumar Dhimal";
+const replace = "    My name is Ram Kumar Dhimal     ";
 
 const replaced = replace.replace("My","His");
 const replaced1 = replace.replaceAll("a", "u");
@@ -77,9 +77,53 @@ console.log(include);
 
 
 // -------------------------------search--------------------------------------
-// acts like the index of and last index of 
+// acts like the indexOf() and lastIndexOf() 
 
 const search = para.search("Ram");
-console.log(search);
+console.log(search); //output of index of given charactar
 
-//output of index of given charactar
+// --------------------------substring-----------------
+const substring = para.substring(2,5);
+console.log(substring); // similar to the slice()
+
+// ---------------------------trim--------------
+
+// its usecase is whenever the user inputs email and password with space before and after the string then this works to remove it
+
+const trim = para.trim();
+console.log(para);
+console.log(trim); ///removes the space in front of the string and after the string
+
+// ---------------------------split-----------------------------------
+
+//split works to make the string, an array to pass it to the data base
+// it is splitting the string 
+const split = para.split(""); //without the space, it splits the string in every characters
+const split1 = para.split(" "); // with space, it splits the string by every words.
+const split2 = para.split("a"); // with certain character, it is splitting in every points where it finds that character
+
+console.log(split);
+console.log(split1);
+console.log(split2);
+
+// -------------------------join-----------------------------------------
+
+// to change that array back to string
+//if i want to join that splitted string, then join is used
+
+const join = split2.join(); //it is joining the splitted string  
+// but it stills separate the words by commas.
+
+console.log(join);
+
+//to change that array back to string then use join with the value of the same separated character 
+//it can be joined by any character as well like:
+const join1 = split2.join("a"); // it is removing all the commas and putting that character back
+
+console.log(join1);
+
+// -------------------------------concat--------------------------------
+// it is the normal concatting process but using the method concat()
+const concat= para.concat(" I live in Sydney.");
+
+console.log(concat);
